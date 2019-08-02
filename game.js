c658458f198ea68625823f4c68be3e82848b675d
monstermania <zeeblee21@gmail.com>
2019-08-02 13:55:58 -0700

    finished tic Tac Toe

--- game.js
+++ game.js
@@ -50,6 +50,7 @@
         var X = 'X';
         var O = 'O';
         var xL = columnA.length;
+        var snorlax = true;
 //X
 
 
@@ -107,6 +108,7 @@ function zero(){
      if(document.getElementById(TableId).innerHTML == ''){
          if(playerX === true) 
            {
+               
              var i = document.getElementById(TableId).innerHTML = X;
             playerX = false;
             document.getElementById('infoTurn').innerHTML = 'O';
@@ -123,126 +125,163 @@ function zero(){
 
     function reset(){
         document.getElementById('resetBtn')
-       var i = document.getElementById(0).innerHTML = "";
-       var i = document.getElementById(1).innerHTML = "";
-       var i = document.getElementById(2).innerHTML = "";
-       var i = document.getElementById(3).innerHTML = "";
-       var i = document.getElementById(4).innerHTML = "";
-       var i = document.getElementById(5).innerHTML = "";
-       var i = document.getElementById(6).innerHTML = "";
-       var i = document.getElementById(7).innerHTML = "";
-       var i = document.getElementById(8).innerHTML = "";
+       document.getElementById(0).innerHTML = "";
+       document.getElementById(1).innerHTML = "";
+       document.getElementById(2).innerHTML = "";
+       document.getElementById(3).innerHTML = "";
+       document.getElementById(4).innerHTML = "";
+       document.getElementById(5).innerHTML = "";
+       document.getElementById(6).innerHTML = "";
+       document.getElementById(7).innerHTML = "";
+       document.getElementById(8).innerHTML = "";
         document.getElementById('youWin').innerHTML = "";
+    }
+    function resetScores(){
+        document.getElementById('XInformationText').innerHTML = 0;
+        document.getElementById('OInformationText').innerHTML = 0;
     }
 
     function CheckWin(){
 
-            if(columnA[0].innerHTML == "X" && columnA[1].innerHTML == "X" && columnA[2].innerHTML == "X"){
-                    
-                    document.getElementById("XInformationText").innerHTML = Xpoints;
-                    Xpoints++;
-                    XWon();
-            }
+            if(columnA[0].innerHTML == "X" && columnA[1].innerHTML == "X" && columnA[2].innerHTML == "X"){   
+                
+                Xpoints++;
+                document.getElementById("XInformationText").innerHTML = Xpoints;
+                setTimeout(() => {
+                XWon();
+            }, 300);
+        }
             else if(columnB[0].innerHTML == "X" && columnB[1].innerHTML == "X" && columnB[2].innerHTML == "X"){
                     
                     document.getElementById("XInformationText").innerHTML = Xpoints;
                     Xpoints++;
+                    setTimeout(() => {
                     XWon();
+                }, 300);
             }
             else if(colomnC[0].innerHTML == "X" && colomnC[1].innerHTML == "X" && colomnC[2].innerHTML == "X"){
                 Xpoints++;
                 document.getElementById("XInformationText").innerHTML = Xpoints;
-                XWon();
+                setTimeout(() => {
+                    XWon();
+                }, 300);
             }
             else if(rowA[0].innerHTML == "X" && rowA[1].innerHTML == "X" && rowA[2].innerHTML == "X"){
                 Xpoints++;
                 document.getElementById("XInformationText").innerHTML = Xpoints;
-                XWon();
+                setTimeout(() => {
+                    XWon();
+                }, 300);
             }
             else if(rowB[0].innerHTML == "X" && rowB[1].innerHTML == "X" && rowB[2].innerHTML == "X"){
                 Xpoints++;
                 document.getElementById("XInformationText").innerHTML = Xpoints;
-                XWon();
+                setTimeout(() => {
+                    XWon();
+                }, 300);
             }
             else if(rowC[0].innerHTML == "X" && rowC[1].innerHTML == "X" && rowC[2].innerHTML == "X"){
                 Xpoints++;
                 document.getElementById("XInformationText").innerHTML = Xpoints;
-                XWon();
+                setTimeout(() => {
+                    XWon();
+                }, 300);
             }
             else if(diagnolA[0].innerHTML == "X" && diagnolA[1].innerHTML == "X" && diagnolA[2].innerHTML == "X"){
                 Xpoints++;
                 document.getElementById("XInformationText").innerHTML = Xpoints;
-                XWon();
+                setTimeout(() => {
+                    XWon();
+                }, 300);
             }
-            else if(rowB[0].innerHTML == "X" && rowB[1].innerHTML == "X" && rowB[2].innerHTML == "X"){
+            else if(diagnolB[0].innerHTML == "X" && diagnolB[1].innerHTML == "X" && diagnolB[2].innerHTML == "X"){
                 Xpoints++;
                 document.getElementById("XInformationText").innerHTML = Xpoints;
-                XWon();
+                setTimeout(() => {
+                    XWon();
+                }, 300);
             }      
             else if(columnA[0].innerHTML == "O" && columnA[1].innerHTML == "O" && columnA[2].innerHTML == "O"){
 
                     Opoints++;
                     document.getElementById("OInformationText").innerHTML = Opoints;
+                    setTimeout(() => {
                     OWon();
+                }, 300);
             }
             else if(columnB[0].innerHTML == "O" && columnB[1].innerHTML == "O" && columnB[2].innerHTML == "O"){
 
                     Opoints++;
                     document.getElementById("OInformationText").innerHTML = Opoints;
+                    setTimeout(() => {
                     OWon();
+                }, 300);
             }
             else if(colomnC[0].innerHTML == "O" && colomnC[1].innerHTML == "O" && colomnC[2].innerHTML == "O"){
                 Opoints++;
                 document.getElementById("OInformationText").innerHTML = Opoints;
-                OWon();
+                setTimeout(() => {
+                    OWon();
+                }, 300);
             }
             else if(rowA[0].innerHTML == "O" && rowA[1].innerHTML == "O" && rowA[2].innerHTML == "O"){
                 Opoints++;
                 document.getElementById("OInformationText").innerHTML = Opoints;
-                OWon();
+                setTimeout(() => {
+                    OWon();
+                }, 300);
             }
             else if(rowB[0].innerHTML == "O" && rowB[1].innerHTML == "O" && rowB[2].innerHTML == "O"){
                 Opoints++;
                 document.getElementById("OInformationText").innerHTML = Opoints;
-                OWon();
+                setTimeout(() => {
+                    OWon();
+                }, 300);
             }
             else if(rowC[0].innerHTML == "O" && rowC[1].innerHTML == "O" && rowC[2].innerHTML == "O"){
                 Opoints++;
                 document.getElementById("OInformationText").innerHTML = Opoints;
-                OWon();
+                setTimeout(() => {
+                    OWon();
+                }, 300);
 
             }
             else if(diagnolA[0].innerHTML == "O" && diagnolA[1].innerHTML == "O" && diagnolA[2].innerHTML == "O"){
                 Opoints++;
                 document.getElementById("OInformationText").innerHTML = Opoints;
-                OWon();
+                setTimeout(() => {
+                    OWon();
+                }, 300);
             }
             else if(diagnolB[0].innerHTML == "O" && diagnolB[1].innerHTML == "O" && diagnolB[2].innerHTML == "O"){
                 Opoints++;
                 document.getElementById("OInformationText").innerHTML = Opoints;
-                OWon();
+                setTimeout(() => {
+                    OWon();
+                }, 300);   
+
             }
-
-        
         }
-
+        
             function XWon()
             {
+
                 alert('X WINS')
             //    document.getElementById('youWin').innerHTML = "X WINS!";         
                     reset();
+                    
             }
-            setTimeout(OWon, 3000);
-
-
-            function OWon(){
-                
-                alert('O WINS')
-               // document.getElementById('youWin').innerHTML = "O WINS!";
-                reset();
-            }
-
-            setTimeout
         
+            
+              
+
+            
+                function OWon(){
+                
+                    alert('O WINS')
+                   // document.getElementById('youWin').innerHTML = "O WINS!";
+                    reset();
+                }
+            
 
         
